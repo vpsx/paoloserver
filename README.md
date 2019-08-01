19ee7c19b5a0ca27769fdd91a02d0bbae47b51e9
vpsx <19900057+vpsx@users.noreply.github.com>
2019-07-31 22:48:29 -0500

    Launch script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # paoloserver
+
+Stick launch.sh into the launch script box when you start your Lightsail instance
+
+Under Networking (for your instance), set the firewall to allow all TCP on all ports
--- launch.sh
+++ launch.sh
@@ -1,1 +1,11 @@
-echo "Hello Lightsail...?!?!"
+# I have no idea what I am doing
+# With help from github.com/mikegcoleman/todo#docker-containers-with-docker-compose
+
+# install latest version of docker the lazy way
+curl -sSL https://get.docker.com | sh
+
+# make it so you don't need to sudo to run docker commands
+usermod -aG docker ubuntu
+
+# run the thing
+docker run -t -p 5000:5000 vpsx/paoloserver:latest
