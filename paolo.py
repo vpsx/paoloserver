71293f01c83d337b9ee30146de7e66e0a99fbaa8
vpsx <19900057+vpsx@users.noreply.github.com>
2019-08-10 16:54:39 -0500

    Add log endpt for testing wild west and east

--- paolo.py
+++ paolo.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def paolo_is_here():
     return render_template('hello.html')
+
+@app.route('/log', methods=['GET', 'POST'])
+def write_log():
+    if request.method == 'POST':
+        if request.is_json:
+            print(request.json)
+        else:
+            print(request.data)
+        return "What am I doing\n"
+    else:
+        return "Only POST for now...\n"
