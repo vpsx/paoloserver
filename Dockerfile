c2d0645fc0921cbf061ee2b7d6eadc0a43387ea7
vpsx <19900057+vpsx@users.noreply.github.com>
2019-08-31 18:07:43 -0500

    sqlalchemy joins the scene

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,19 @@
 FROM alpine:3.9.4
 
 # Add a bunch of nice stuff; apologies to image size
-RUN apk update && apk add --no-cache bash vim python3
+# Upd: Blerghh alpine doesn't like psycopg2-binary/any wheel
+# see https://github.com/psycopg/psycopg2/issues/684
+# and initd.org/psycopg/docs/install.html#install-from-source
+# So must add gcc, python3-dev, ...what is postgresql-dev... musl... what
+# upd: jk there's py3-psycopg2 idk I'll use it
+RUN apk update && apk add --no-cache bash vim python3 py3-psycopg2
 
 # Apparently this is good practice. Still no good grasp of why tbh.
 RUN adduser -D paolo
 WORKDIR /home/paolo
 
 # Stuff...
-RUN pip3 install flask
+RUN pip3 install flask sqlalchemy
 ENV FLASK_APP=paolo.py
 ADD paolo.py .
 ADD templates/hello.html templates/hello.html
--- paolo.py
+++ paolo.py
@@ -1,7 +1,50 @@
+from contextlib import contextmanager
+
 from flask import Flask
 from flask import render_template
 from flask import request
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# dialect+driver://username:password@host:port/database
+engine = create_engine('postgresql+psycopg2://aolop:notexposedongithub@db:5432/paolosdb', echo=False)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+class Paolo(Base):
+    __tablename__ = 'paolos'
+
+    # todo: Add unique constraint on name probably
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    verysecretsecret = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    birthday = Column(Integer)
+
+Base.metadata.create_all(engine)
+
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def paolo_is_here():
@@ -17,3 +60,33 @@ def write_log():
         return "What am I doing\n"
     else:
         return "Only POST for now...\n"
+
+
+
+def add_some_stuff():
+
+    flamingo = Paolo(
+        name='flamingo',
+        verysecretsecret='onthelake',
+        latitude=41.794722,
+        longitude=-87.580833,
+        birthday=19270000,
+    )
+
+    cove = Paolo(
+        name='cove',
+        latitude=41.79543,
+        longitude=-87.5819805,
+    )
+
+    tadpoles = Paolo(
+        name='tadpoles',
+        latitude=41.795363,
+        longitude=-87.578011,
+    )
+
+    with session_scope() as session:
+        session.add_all([flamingo, cove, tadpoles])
+
+add_some_stuff()
+print("Tada")
