3c37d424574abc167f8e1c053fb4e27463ae412a
vpsx <19900057+vpsx@users.noreply.github.com>
2019-09-02 15:07:33 -0500

    Install compose on launch, tweak compose yml

--- launch.sh
+++ launch.sh
@@ -7,5 +7,10 @@ curl -sSL https://get.docker.com | sh
 # make it so you don't need to sudo to run docker commands
 usermod -aG docker ubuntu
 
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # run the thing
-docker run -t -p 80:5000 vpsx/paoloserver:latest
+# docker run -t -p 80:5000 vpsx/paoloserver:latest
+docker-compose up -d
