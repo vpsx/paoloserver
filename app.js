6a23baad450059d9c79c871af0a49134d7facb36
vpsx <19900057+vpsx@users.noreply.github.com>
2021-11-15 13:10:49 +0100

    Rewrite everything with websockets

---
+++ app.js
@@ -0,0 +1,17 @@
+const { WebSocket, WebSocketServer } = require('ws');
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.send('Connection established; broadcasting messages');
+});
