c7884340323f9daabcaa2cc2cd7d788f65089017
vpsx <19900057+vpsx@users.noreply.github.com>
2021-11-16 12:23:09 +0100

    Fix message forwarding

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const { WebSocket, WebSocketServer } = require('ws');
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', function message(data, isBinary) {
+    console.log('received: %s', data);
 
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        console.log('forwarding: %s', data);
+        client.send(data, { binary: isBinary });
       }
     });
   });
