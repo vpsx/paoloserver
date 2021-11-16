03d62f36d836abe89cd33af40f22f9ab26f66b33
vpsx <19900057+vpsx@users.noreply.github.com>
2021-11-16 12:48:04 +0100

    Log IP of message sender/source

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const { WebSocket, WebSocketServer } = require('ws');
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
+  const ip = req.socket.remoteAddress;
+
   ws.on('message', function message(data, isBinary) {
-    console.log('received: %s', data);
+    console.log(`received from ${ip}: ${data}`);
 
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        console.log('forwarding: %s', data);
+        console.log(`forwarding: ${data}`);
         client.send(data, { binary: isBinary });
       }
     });
